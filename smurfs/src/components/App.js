92f082f6cc0ac4544c95695933f00254b6251220
remoo1901 <remoo1901@hotmail.com>
2020-05-01 20:16:20 -0400

    finished contexts

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import SmurfsContext from "./contexts/SmurfsContext"
 
 import "./App.css";
 
@@ -29,12 +30,14 @@ const App = () => {
 
   return (
     <div className="App">
+    <SmurfsContext.Provider>
       <h1>SMURFS! 2.0 W/ Redux</h1>
       <div>Welcome to your state management version of Smurfs!</div>
       <div>Start inside of your `src/index.js` file!</div>
       <div>Have fun!</div>
       <SmurfsForm />
       <SmurfsList />
+      </SmurfsContext.Provider>
     </div>
   );
 };
---
+++ smurfs/src/components/contexts/SmurfsContext.js
@@ -0,0 +1,6 @@
+import {createContext} from "react"
+
+const SmurfsContext = createContext()
+
+
+export default SmurfsContext;
